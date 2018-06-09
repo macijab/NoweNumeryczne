681d420cab360235901f205787fa7e857937df08
Maciej Jablonski <maciejijablonski@gmail.com>
2018-06-09 17:08:50 +0200

    Initial commit

---
+++ src/bialko/Hipergeometryczny.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bialko;
+
+/**
+ *
+ * @author faust
+ */
+public class Hipergeometryczny {
+    int M,k,N,n;
+    public Hipergeometryczny(int a,int b,int c,int d){
+        this.M = a;
+        this.k = b;
+        this.N = c;
+        this.n = d;
+    }
+    
+    
+}
---
+++ src/bialko/Poisson.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bialko;
+
+/**
+ *
+ * @author faust
+ */
+
+
+public class Poisson {
+    private double dbLambda;
+    public static int getPoisson(double dbLambda) {
+        double L = Math.exp(-dbLambda);
+        double p = 1.0;
+        int k = 0;
+
+        do {
+            k++;
+            p *= Math.random();
+        } while (p > L);
+
+    return k - 1;
+    }
+}
